ecd71273794a4f25aac1832213f4a3842d5f1ee5
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-08-12 16:44:15 +0200

    Merge everything in one file

--- srfsp.py
+++ srfsp.py
@@ -2,7 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import pyunlocbox
-from tools import plotfftreal
+
+
+def plotfftreal(s, fs, dataset, amp='abs'):
+    r"""
+    Plot the Fourier transform in a nice way.
+
+    Parameters
+    ----------
+    s : array_like
+        Fourier transform of a signal.
+    fs : float
+        Sampling frequency.
+    dataset : string
+        Dataset name.
+    amp : {'abs', 'real', 'imag'}
+        Type of amplitude to plot.
+    """
+    N = len(s)
+    w = np.linspace(0, fs-fs/N, N)
+
+    exec('y = np.%s(s)' % (amp,))
+
+    plt.plot(w, y, 'b.-')
+
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude (%s)' % (amp,))
+
+    if dataset is 'myoglobin':
+        plt.xlim(938.5e3, 941.5e3)
+
+#    plt.show()
 
 
 def artificial():
