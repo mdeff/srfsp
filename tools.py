5d59b1da8f73a2056b282976e6a21fba6384cb9e
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-08-12 11:55:28 +0200

    No stem plots

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plotfftreal(s, fs, stem=False):
+def plotfftreal(s, fs):
     r"""
     Plot the Fourier transform in a nice way.
 
@@ -12,18 +12,11 @@ def plotfftreal(s, fs, stem=False):
         The Fourier transform of a signal.
     fs : float
         The sampling frequency
-    stem : bool, optional
-        Use stem plot if ``True``, normal plot otherwise.
     """
     N = len(s)
     w = np.linspace(0, fs-fs/N, N)
 
-    if stem:
-        plot = plt.stem
-    else:
-        plot = plt.plot
-
-    plot(w, np.abs(s), 'b.-')
+    plt.plot(w, np.abs(s), 'b.-')
 
     plt.xlabel('Frequency [Hz]')
     plt.ylabel('Amplitude')
